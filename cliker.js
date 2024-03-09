a0e2c39dd26aab989be0e7db4eadc48d718d6cf5
julien44830 <deniaudjulien0@gmail.com>
2024-03-09 01:33:05 +0100

    modify footer and add clicker

--- cliker.js
+++ cliker.js
@@ -1,6 +1,7 @@
 const resulta = document.getElementsByClassName('resulta');
 const clik = document.getElementsByClassName('img');
 const body = document.getElementsByClassName('body');
+let clic =0;
 let num = 0;
 
 console.log(clik, resulta, body);
@@ -12,9 +13,13 @@ function cunter() {
 // Boucle à travers chaque élément avec la classe 'img' et ajoute l'écouteur d'événements
 for (let i = 0; i < clik.length; i++) {
     clik[i].addEventListener("click", (e) => {
+        const score = document.querySelector('.score');
+        let scorePara = document.getElementsByClassName('scorePara');
+        let message = "tu a usé ta sourie de " + clic + " click pour rien";
+        let scoreMessae = "ton score est de " + num;
+        clic += 1;
         num += 1;
         console.log('valeur du click : ' + num);
-        let message = "tu a usé ta sourie de " + num + " click pour rien";
         console.log(message);
 
         if(num >= 10){
@@ -22,6 +27,10 @@ for (let i = 0; i < clik.length; i++) {
 
             document.body.classList.add('badground');
             document.body.style.backgroundColor = 'red'; 
+            score.classList.add('score');
+            score.style.display = 'flex'; 
+            scorePara[i].innerHTML = scoreMessae;
+    
         };
         if(num>=100){
             num += 2;
@@ -39,13 +48,14 @@ for (let i = 0; i < clik.length; i++) {
             alert('fait une pose avec ta sourie bourin');
         }
 
-
         // Parcourir chaque élément de la collection resulta
         for (let j = 0; j < resulta.length; j++) {
             // Mettre à jour le contenu HTML de chaque élément avec la valeur de num
             resulta[j].innerHTML = message;
         }
     });
-}
+};
+
+
 
 console.log( resulta);
